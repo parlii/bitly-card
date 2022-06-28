0d53d4dd40c7b1ea631c934f4d95131831a8eae9
Colin Hemphill <colin.hemphill@bit.ly>
2022-06-28 11:38:01 -0500

    Refactor to use context to store results of shorten API and display views instead of routing to a new page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react';
 import GenerateLink from '../components/GenerateLink/GenerateLink';
+import ShareCard from '../components/ShareCard/ShareCard';
+import { ShareCardContext } from '../context/ShareCardContext';
 
 const Page: React.FC = () => {
+  const { shareCard } = useContext(ShareCardContext);
+
   return (
     <div className="generate-link-page d-flex w-100 align-items-center">
       <div className="col">
         <div className="container">
-          {/* <h1 className="text-center mb-5">Bitly Card</h1> */}
-          <GenerateLink />
+          {!shareCard && <GenerateLink />}
+          {shareCard && <ShareCard />}
         </div>
       </div>
     </div>
