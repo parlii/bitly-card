bc7866dcf979db9358084d0074d7e39e85b4604f
Parli Rizal <74726025+parlii@users.noreply.github.com>
2022-06-28 14:16:52 -0400

    added API call to shorten

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,8 +1,64 @@
 import { NextApiHandler } from 'next';
-import { ShareCard } from '../../context/ShareCardContext';
+import { Url } from 'url';
 
-const handler: NextApiHandler<ShareCard> = (req, res) => {
+var axios = require('axios');
+
+const ShortenUrl = async (destinationUrl: Url) => {
+  var payload = {
+    group_guid: `${process.env.DEFAULT_GROUP_GUID}`,
+    long_url: destinationUrl,
+    domain: 'bit.ly',
+  };
+
+  var config = {
+    method: 'POST',
+    url: 'https://api-ssl.bitly.com/v4/shorten',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.BITLY_ACCESS_TOKEN}`,
+    },
+    data: JSON.stringify(payload),
+  };
+
+  const response = await axios(config);
+
+  return await response;
+};
+
+// const GenerateQR = (url: Url) => {
+//   var payload = {
+//     group_guid: 'BlcemOt3jWM',
+//     long_url: destinationUrl,
+//     domain: 'bit.ly',
+//   };
+
+//   var config = {
+//     method: 'POST',
+//     url: 'https://api-ssl.bitly.com/v4/shorten',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: 'Bearer 891fde1d4190877eab0b1f1e12673cdbbe169d44',
+//     },
+//     data: JSON.stringify(payload),
+//   };
+
+//   axios(config)
+//     .then(function (response) {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// };
+
+const handler: NextApiHandler = async (req, res) => {
   const body = JSON.parse(req.body);
+
+  const shortenedUrl = await ShortenUrl(body.destinationUrl);
+
+  console.log(shortenedUrl);
+
+  // const qrCode = GenerateQR(`http://bit.ly/parli`);
 
   const testResponse = {
     backhalf: 'abc123',
