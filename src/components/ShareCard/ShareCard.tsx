20719f19432ee0644849be24b68b7e582062d38c
Colin Hemphill <colinhemphill@users.noreply.github.com>
2022-06-29 14:56:38 -0500

    Add functionality to donwload the card as a PNG. (#8)

--- src/components/ShareCard/ShareCard.tsx
+++ src/components/ShareCard/ShareCard.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image';
-import React, { useContext, useState } from 'react';
+import classNames from 'classnames';
+import { toPng } from 'html-to-image';
+import Image from 'next/future/image';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { ShareCardContext } from '../../context/ShareCardContext';
 import { BitlyLogo } from '../BitlyLogo/BitlyLogo';
 import { copyToClipboard } from '../utils/copy';
@@ -15,6 +17,17 @@ const ShareCard: React.FC = () => {
     shareCard: { backhalf, domain, destinationDomain, qr },
   } = useContext(ShareCardContext);
   const [copyConfirm, setCopyConfirm] = useState<boolean>(false);
+  const [imageData, setImageData] = useState<string>();
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const element = cardRef.current;
+    const loadCanvas = async () => {
+      const imageDataUrl = await toPng(element);
+      setImageData(imageDataUrl);
+    };
+    loadCanvas();
+  }, []);
 
   const showCopyAlert = () => {
     setCopyConfirm(true);
@@ -37,44 +50,60 @@ const ShareCard: React.FC = () => {
   };
 
   return (
-    <div
-      className={`share-page share-page--theme-${theme} d-flex align-items-center justify-content-center`}
-    >
-      <div className="share-page__card rounded d-flex align-items-center justify-content-center">
-        <div className="share-page__bitly-logo">
-          <BitlyLogo />
-        </div>
-        <div className="share-page__qr-code">
+    <>
+      <div
+        className={classNames(
+          'share-page d-flex align-items-center justify-content-center',
+          `share-page--theme-${theme}`,
+        )}
+        ref={cardRef}
+      >
+        <div className="share-page__card rounded d-flex align-items-center justify-content-center">
+          <div className="share-page__bitly-logo">
+            <BitlyLogo />
+          </div>
+
           <Image
             alt={`QR code that leads to ${domain}/${backhalf}`}
-            className="rounded"
+            className="share-page__qr-code"
             height={300}
             src={qr}
             width={300}
           />
+
+          <button
+            className={classNames('share-page__link-info px-5 py-2 rounded', {
+              'share-page__link-info--copy-confirm': copyConfirm,
+            })}
+            onClick={() => {
+              copyLink();
+            }}
+            type="button"
+          >
+            <div className="share-page__link-info__link-safety">
+              <VerifiedLink />
+            </div>
+            <div className="share-page__link-info__domain">{domain}</div>
+            <div className="share-page__link-info__backhalf">
+              {copyConfirm ? 'Copied!' : backhalf}
+            </div>
+            <div className="share-page__link-info__forwards-to">
+              <em>Forwards to {destinationDomain}/...</em>
+            </div>
+          </button>
         </div>
-        <button
-          className={`share-page__link-info px-5 py-2 rounded ${
-            copyConfirm ? 'share-page__link-info--copy-confirm' : ''
-          }`}
-          onClick={() => {
-            copyLink();
-          }}
-          type="button"
-        >
-          <div className="share-page__link-info__link-safety">
-            <VerifiedLink />
-          </div>
-          <div className="share-page__link-info__domain">{domain}</div>
-          <div className="share-page__link-info__backhalf">
-            {copyConfirm ? 'Copied!' : backhalf}
-          </div>
-          <div className="share-page__link-info__forwards-to">
-            <em>Forwards to {destinationDomain}/...</em>
-          </div>
-        </button>
       </div>
-    </div>
+
+      {imageData && (
+        <a
+          className="btn btn-dark"
+          download={`${backhalf}.png`}
+          href={imageData}
+        >
+          Download
+        </a>
+      )}
+    </>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,34 @@
+import debounce from 'awesome-debounce-promise';
 import 'css-doodle';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import BackgroundSwitch from '../components/BackgroundSwitch/BackgroundSwitch';
 import { ShareCard, ShareCardContext } from '../context/ShareCardContext';
 import '../styles/index.scss';
 
+const setViewportHeight = (): void => {
+  // fix viewport height inconsistency on mobile browsers with address bar
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
 const MyApp = ({ Component, pageProps }) => {
   const [shareCard, setShareCard] = useState<ShareCard>();
+
+  const updateWindowData = useRef(
+    debounce(() => {
+      setViewportHeight();
+    }, 300),
+  );
+
+  useEffect(() => {
+    const handler = updateWindowData.current;
+    handler();
+
+    window.addEventListener('resize', handler);
+    return () => {
+      window.removeEventListener('resize', handler);
+    };
+  }, []);
 
   return (
     <ShareCardContext.Provider value={{ shareCard, setShareCard }}>
